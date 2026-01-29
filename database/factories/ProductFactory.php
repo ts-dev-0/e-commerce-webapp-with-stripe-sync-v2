3306ee7589c6adfae2ef87b00e339c4c598d9e6f
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-01-29 16:21:25 +0900

    Add ProductFactory class

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->realText(20),
+            'description' => $this->faker->realText(200),
+            'price' => $this->faker->numberBetween(100, 100000),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'created_at' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'updated_at' => now(),
+        ];
+    }
+}
