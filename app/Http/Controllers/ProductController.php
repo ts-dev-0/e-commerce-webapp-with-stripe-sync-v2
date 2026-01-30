3cc4da7051eeaf325c60400d1a386a6d1c063c86
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-01-30 20:19:56 +0900

    feat: implement product detail browsing feature

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function show(Product $product)
+    {
+        return Inertia::render('product/show', [
+            'product' => $product,
+        ]);
+    }
+}
