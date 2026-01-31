b7a48b9f847cb597d8627a8fee8a99f236569328
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-01-31 21:51:52 +0900

    test: add test for current cart creation when it does not exist

--- tests/Feature/Models/User/UserCartTest.php
+++ tests/Feature/Models/User/UserCartTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models\User;
 
+use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -19,5 +20,20 @@ class UserCartTest extends TestCase
             \Illuminate\Database\Eloquent\Relations\HasOne::class,
             $user->cart()
         );
+    }
+
+    public function test_current_cart_creates_cart_if_not_exists()
+    {
+        $user = User::factory()->create();
+
+        $cart = $user->currentCart();
+
+        $this->assertDatabaseHas('carts', [
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertEquals($user->id, $cart->user_id, 'Cart user_id should match the logged in user id');
+
+        $this->assertEquals(1, Cart::where('user_id', $user->id)->count(), 'There should be exactly one cart created for the user');
     }
 }
