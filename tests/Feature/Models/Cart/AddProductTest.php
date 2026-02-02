1a77dfd6a00cae89578d347b550553b6291ca2b4
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-02 21:28:19 +0900

    fix: change test class name

--- tests/Feature/Models/Cart/AddProductTest.php
+++ tests/Feature/Models/Cart/AddProductTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CartAddProductTest extends TestCase
+class AddProductTest extends TestCase
 {
     use RefreshDatabase;
 
