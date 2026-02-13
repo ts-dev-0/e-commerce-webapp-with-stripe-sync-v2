72632d6163f7f4354cd833fca94ca885b8606e97
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-13 16:47:28 +0900

    feat: add alreadyReviewed method

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -20,4 +20,11 @@ class Review extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    public static function alreadyReviewed(int $userId, int $productId): bool
+    {
+        return static::where('user_id', $userId)
+            ->where('product_id', $productId)
+            ->exists();
+    }
 }
