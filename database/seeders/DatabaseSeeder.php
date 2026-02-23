02f8352d555562f12f45f0ae22e89342f1ce0dc3
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-23 17:31:24 +0900

    feat: add CategoryProductSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             CategorySeeder::class,
+            CategoryProductSeeder::class,
         ]);
     }
 }
