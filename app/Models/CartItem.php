ce28672dc7cee137c5d1d96c0e6708507114cd59
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-23 21:58:54 +0900

    fix: remove blank in CarItem

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'cart_id',
         'product_id',
