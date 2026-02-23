264788bd1b968997be2605fd4e643bf42393e691
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-23 22:01:23 +0900

    feat: add CartItemSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             CategorySeeder::class,
             CategoryProductSeeder::class,
+            CartItemSeeder::class,
         ]);
     }
 }
