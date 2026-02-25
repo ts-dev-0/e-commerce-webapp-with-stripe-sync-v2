9feca610f731771e6bbfe188df75ff9a25c4b5d3
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-25 12:38:43 +0900

    feat: add HomeController, home path and home page

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products/{product}', [ProductController::class, 'show']);
-
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
