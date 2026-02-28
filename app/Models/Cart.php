68331b740a6a2e9b0d9abaf067fefca3186e11a6
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-28 16:31:36 +0900

    ref: delete cart action code from cart model and the corresponding tests

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -26,53 +26,6 @@ class Cart extends Model
         return $this->hasMany(CartItem::class);
     }
 
-    public function addProduct(Product $product, int $quantity = 1): void
-    {
-        if ($quantity < 1) {
-            throw new \InvalidArgumentException('Quantity must be at least 1.');
-        }
-
-        $existing = $this->products()
-            ->where('product_id', $product->id)
-            ->first();
-
-        if($existing) {
-            $this->products()->updateExistingPivot(
-                $product->id,
-                [
-                    'quantity' => $existing->pivot->quantity + $quantity,
-                ]
-            );
-        }
-
-        $this->products()->attach($product->id, [
-            'quantity' => $quantity,
-        ]);
-    }
-
-    public function updateProductQuantity(Product $product, int $updatedQuantity): void
-    {
-        if($updatedQuantity < 1) {
-            throw new \InvalidArgumentException('Quantity must be at least 1.');
-        }
-
-        if (! $this->products()->where('product_id', $product->id)->exists()) {
-            throw new \InvalidArgumentException('Product does not exist.');
-        }
-
-        $this->products()->updateExistingPivot(
-            $product->id,
-            [
-                'quantity' => $updatedQuantity,
-            ]
-        );
-    }
-
-    public function removeProduct(Product $product): void
-    {
-        $this->products()->detach($product->id);
-    }
-
     public function checkout(): Order
     {
         $products = $this->products;
