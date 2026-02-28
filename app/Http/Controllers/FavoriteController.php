4dabe56fa25c55178b5070701a88191f43c5020c
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-28 21:00:23 +0900

    feat: add action method to FavoriteController

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,22 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\User\Favorite\AddFavorite;
+use App\Actions\User\Favorite\RemoveFavorite;
+use App\Actions\User\Favorite\ViewFavoriteProducts;
 use Inertia\Inertia;
 
 class FavoriteController extends Controller
 {
-    public function index()
+    public function index(ViewFavoriteProducts $action)
     {
-        return Inertia::render('favorite-products');
+        $data = $action->handle($user);
+
+        return Inertia::render('favorite-products', [
+            'data' => $data,
+        ]);
     }
 
-    public function store()
+    public function store(AddFavorite $action)
     {
+        $action->handle($user, $product);
+
         return redirect()->back()->with('success', 'Added to favorite.');
     }
 
-    public function destroy()
+    public function destroy(RemoveFavorite $action)
     {
+        $action->handle($user, $product);
+
         return redirect()->back()->with('success', 'Removed favorite.');
     }
 }
