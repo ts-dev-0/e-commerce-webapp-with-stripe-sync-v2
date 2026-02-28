6f84e72bae824d889e922818eb84690c8783c59c
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-02-28 20:43:58 +0900

    feat: add action handle method in CartController

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\User\Cart\AddProductToCart;
 use App\Actions\User\Cart\GetCart;
+use App\Actions\User\Cart\RemoveCartItem;
+use App\Actions\User\Cart\UpdateProductQuantity;
 use Inertia\Inertia;
 
 class CartController extends Controller
@@ -16,22 +19,28 @@ class CartController extends Controller
         ]);
     }
 
-    public function store()
+    public function store(AddProductToCart $action,)
     {
+        $action->handle($user, $product, $quantity);
+
         return redirect()
             ->route('cart.index')
             ->with('success', 'Product added to cart.');
     }
 
-    public function update()
+    public function update(UpdateProductQuantity $action)
     {
+        $action->handle($user, $product, $quantity);
+
         return redirect()
             ->route('cart.index')
             ->with('success', 'Cart updated.');
     }
 
-    public function destroy()
+    public function destroy(RemoveCartItem $action)
     {
+        $action->handle($user, $product);
+
         return redirect()
             ->route('cart.index')
             ->with('success', 'Item removed from cart.');
