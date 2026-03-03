2d2b8099697091f01d1e0edc2525d318d58a621b
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-03 10:20:09 +0900

    feat: add AddItemToCartRequest class and the corrensponding tests

---
+++ tests/Feature/Requests/User/Cart/AddItemToCartRequestTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests\Feature\Requests\User\Cart;
+
+use App\Http\Requests\User\Cart\AddItemToCartRequest;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class AddItemToCartRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private AddItemToCartRequest $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->request = new AddItemToCartRequest();
+    }
+
+    public function test_validation_passes_with_valid_data()
+    {
+        $product = Product::factory()->create();
+
+        $validator = Validator::make([
+            'product_id' => $product->id,
+            'quantity' => 1,
+        ], $this->request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function test_validation_fails_when_product_id_is_missing()
+    {
+        Product::factory()->create();
+
+        $validator = Validator::make([
+            'quantity' => 1,
+        ], $this->request->rules());
+
+        $this->assertFalse($validator->passes());
+        $this->assertArrayHasKey('product_id', $validator->errors()->toArray());
+    }
+
+    public function test_validation_fails_when_quantity_is_missing()
+    {
+        $product = Product::factory()->create();
+
+        $request = new AddItemToCartRequest();
+
+        $validator = Validator::make(
+            [
+                'product_id' => $product->id,
+            ],
+            $request->rules()
+        );
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('quantity', $validator->errors()->toArray());
+    }
+
+    public function test_validation_fails_when_quantity_is_less_than_one()
+    {
+        $product = Product::factory()->create();
+
+        $request = new AddItemToCartRequest();
+
+        $validator = Validator::make(
+            [
+                'product_id' => $product->id,
+                'quantity' => 0,
+            ],
+            $request->rules()
+        );
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('quantity', $validator->errors()->toArray());
+    }
+
+    public function test_validation_fails_when_quantity_is_more_over_than_99()
+    {
+        $product = Product::factory()->create();
+
+        $request = new AddItemToCartRequest();
+
+        $validator = Validator::make(
+            [
+                'product_id' => $product->id,
+                'quantity' => 100,
+            ],
+            $request->rules()
+        );
+
+        $this->assertTrue($validator->fails());
+        $this->assertArrayHasKey('quantity', $validator->errors()->toArray());
+    }
+}
