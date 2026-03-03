1d640855312989f36c1ce03cdedfb9a370f933c8
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-03 10:29:15 +0900

    feat: add UpdateCartItemQuantityRequest and the corresponding tests

---
+++ app/Http/Requests/Requests/User/Cart/UpdateCartItemQuantityRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Requests\User\Cart;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCartItemQuantityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => ['bail', 'required', 'integer', 'exists:products,id'],
+            'quantity' => ['required', 'integer', 'min:1', 'max:99'],
+        ];
+    }
+}
