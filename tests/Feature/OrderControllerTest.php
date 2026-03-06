b2d8960e5317a11cbd53cffb265d62ffbed1d378
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-06 13:22:36 +0900

    test: add cancel method in OrderController

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -5,8 +5,9 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Mockery;
 use App\Models\User;
-use App\Actions\User\Order\ViewOrderHistory;
 use App\Models\Order;
+use App\Actions\User\Order\CancelOrder;
+use App\Actions\User\Order\ViewOrderHistory;
 use Inertia\Testing\AssertableInertia as Assert;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -21,6 +22,7 @@ class OrderControllerTest extends TestCase
         $this->withoutVite();
     }
 
+    // index
     public function test_authenticated_user_can_view_order_history()
     {
         /** @var \App\Models\User $user */
@@ -59,6 +61,41 @@ class OrderControllerTest extends TestCase
     {
         $response = $this->get(route('orders.index'));
 
+        $response->assertRedirect(route('login'));
+    }
+
+    // cancel
+    public function test_user_can_cancel_order()
+    {
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $order = Order::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $mock = Mockery::mock(CancelOrder::class);
+
+        $mock->shouldReceive('handle')
+            ->once()
+            ->with(Mockery::type(Order::class));
+
+        $this->app->instance(CancelOrder::class, $mock);
+
+        $response = $this
+            ->actingAs($user)
+            ->post(route('orders.cancel', $order));
+
+        $response->assertRedirect(route('orders.index'));
+        $response->assertSessionHas('success', 'Order has been cancelled.');
+    }
+
+    public function test_guest_cannot_cancel_order()
+    {
+        $order = Order::factory()->create();
+
+        $response = $this->post(route('orders.cancel', $order));
+        
         $response->assertRedirect(route('login'));
     }
 }
