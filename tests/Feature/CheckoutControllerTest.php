c31f03d9d4ed528e141aed79f6af98f16f7b32aa
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-06 13:36:16 +0900

    test: add index method in CheckoutController

---
+++ tests/Feature/CheckoutControllerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Mockery;
+use App\Models\User;
+use App\Actions\User\Cart\GetCart;
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\Product;
+use Inertia\Testing\AssertableInertia as Assert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheckoutControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
+
+    public function test_authenticated_user_can_view_checkout_page()
+    {
+        /** @var \APP\Models\User $user */
+        $user = User::factory()->create();
+
+        $cart = Cart::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $product1 = Product::factory()->create([
+            'is_published' => 1,
+        ]);
+        $product2 = Product::factory()->create([
+            'is_published' => 0,
+        ]);
+
+        $data = [
+            [
+                'product' => $product1->toArray(),
+                'quantity' => 2,
+            ],
+            [
+                'product' => $product2->toArray(),
+                'quantity' => 1,
+            ]
+            ];
+
+        $mock = Mockery::mock(GetCart::class);
+
+        $mock->shouldReceive('handle')
+            ->once()
+            ->with($user)
+            ->andReturn($data);
+
+        $this->app->instance(GetCart::class, $mock);
+
+        $response = $this
+            ->actingAs($user)
+            ->get(route('checkout.index'));
+
+        $response->assertOk();
+
+        $response->assertInertia(fn (Assert $page) =>
+            $page->component('checkout')
+                 ->where('data', $data)
+        );
+    }
+
+    public function test_guest_cannot_access_checkout_page()
+    {
+        $response = $this->get(route('checkout.index'));
+
+        $response->assertRedirect('/login');
+    }
+}
