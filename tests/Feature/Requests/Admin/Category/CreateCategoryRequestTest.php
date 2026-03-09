b21375ddc170bda8103e3eab50403726ac84f2e8
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-09 13:54:39 +0900

    feat: add CreateCategoryRequest and the corresponding tests

---
+++ tests/Feature/Requests/Admin/Category/CreateCategoryRequestTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Requests\Admin\Category;
+
+use App\Http\Requests\Admin\Category\CreateCategoryRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class CreateCategoryRequestTest extends TestCase
+{
+    private CreateCategoryRequest $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->request = new CreateCategoryRequest();
+    }
+
+    public function test_validation_passes_with_valid_data()
+    {
+        $validator = Validator::make([
+            'name' => 'Test category',
+        ], $this->request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function test_validation_fails_when_name_field_is_not_exists()
+    {
+        $validator = Validator::make([], $this->request->rules());
+
+        $this->assertTrue($validator->fails());
+    }
+
+    public function test_validation_fails_when_name_is_integer()
+    {
+        $validator = Validator::make([
+            'name' => 1,
+        ], $this->request->rules());
+
+        $this->assertTrue($validator->fails());
+    }
+
+    public function test_validation_fails_when_name_is_more_than_255()
+    {
+        $validator = Validator::make([
+            'name' => str_repeat('a', 256),
+        ], $this->request->rules());
+
+        $this->assertTrue($validator->fails());
+    }
+}
