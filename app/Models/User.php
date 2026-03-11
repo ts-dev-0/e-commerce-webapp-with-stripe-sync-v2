e6b21ae24f335a4911f024472e67805e1c85433b
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-11 19:29:14 +0900

    feat: add is_admin column to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
+            'is_admin' => 'boolean',
         ];
     }
 
