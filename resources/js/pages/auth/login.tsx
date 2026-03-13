0216124d497dd935fb3f551ebcba7ad6f5492506
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-13 12:15:48 +0900

    feat: add login and register

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,7 +1,77 @@
-export default function Login() {
+import TextLink from '@/components/text-link';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Spinner } from '@/components/ui/spinner';
+import AuthLayout from '@/layouts/auth-layout';
+import { register } from '@/routes';
+import { store } from '@/routes/login';
+import { Form, Head } from '@inertiajs/react';
+
+interface LoginProps {
+    canRegister: boolean;
+}
+
+export default function Login({ canRegister }: LoginProps) {
     return (
-        <div>
-            <div>Login Page</div>
-        </div>
+        <AuthLayout
+            title="Log in to your account"
+            description="Enter your email and password below to log in"
+        >
+            <Head title="Log in" />
+
+            <Form {...store.form()} className="flex flex-col gap-6">
+                {({ processing }) => (
+                    <>
+                        <div className="grid gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email address</Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    autoFocus
+                                    tabIndex={1}
+                                    autoComplete="email"
+                                    placeholder="email@example.com"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="item-center flex">
+                                    <Label htmlFor="password">Password</Label>
+                                </div>
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    tabIndex={2}
+                                    autoComplete="current-password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <Button
+                                type="submit"
+                                className="mt-4 w-full"
+                                tabIndex={3}
+                                disabled={processing}
+                            >
+                                {processing && <Spinner />}
+                                Log in
+                            </Button>
+                        </div>
+                        {canRegister && (
+                            <div className="text-muted-foregraund text-center text-sm">
+                                Don't have an account?{' '}
+                                <TextLink href={register()} tabIndex={4}>
+                                    Sign in
+                                </TextLink>
+                            </div>
+                        )}
+                    </>
+                )}
+            </Form>
+        </AuthLayout>
     );
 }
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -1,7 +1,99 @@
+import TextLink from '@/components/text-link';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Spinner } from '@/components/ui/spinner';
+import AuthLayout from '@/layouts/auth-layout';
+import { login } from '@/routes';
+import { store } from '@/routes/register';
+import { Form, Head } from '@inertiajs/react';
+
 export default function Register() {
     return (
-        <div>
-            <div>Register Page</div>
-        </div>
+        <AuthLayout
+            title="Create an account"
+            description="Enter your details below to create your account"
+        >
+            <Head title="Register" />
+
+            <Form {...store.form()} className="flex flex-col gap-6">
+                {({ processing }) => (
+                    <>
+                        <div className="grid gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Name</Label>
+                                <Input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    required
+                                    autoFocus
+                                    tabIndex={1}
+                                    placeholder="email@example.com"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email address</Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    tabIndex={2}
+                                    autoComplete="email"
+                                    placeholder="email@example.com"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="item-center flex">
+                                    <Label htmlFor="password">Password</Label>
+                                </div>
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    tabIndex={3}
+                                    autoComplete="current-password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="item-center flex">
+                                    <Label htmlFor="password">
+                                        Confirm Password
+                                    </Label>
+                                </div>
+                                <Input
+                                    id="password_confirmation"
+                                    name="password_confirmation"
+                                    type="password"
+                                    required
+                                    tabIndex={4}
+                                    autoComplete="new-password"
+                                    placeholder="Confirm password"
+                                />
+                            </div>
+                            <Button
+                                type="submit"
+                                className="mt-4 w-full"
+                                tabIndex={5}
+                                disabled={processing}
+                            >
+                                {processing && <Spinner />}
+                                Create account
+                            </Button>
+                        </div>
+
+                        <div className="text-muted-foregraund text-center text-sm">
+                            Already have an account?{' '}
+                            <TextLink href={login()} tabIndex={6}>
+                                Log in
+                            </TextLink>
+                        </div>
+                    </>
+                )}
+            </Form>
+        </AuthLayout>
     );
 }
