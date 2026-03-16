37376d0f36c4018b41af08e2fe6fb08ac6748c35
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-16 20:29:37 +0900

    feat: add action class to get chckout data

---
+++ app/Actions/User/Checkout/GetCheckout.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\User\Checkout;
+
+use App\Models\CartItem;
+use App\Models\User;
+
+class GetCheckout
+{
+    public function handle(User $user): array
+    {
+        $cartItems = CartItem::with('product')
+            ->whereHas('cart', function ($query) use ($user) {
+                $query->where('user_id', $user->id);
+            })
+            ->get();
+
+        return $cartItems->map(function ($item) {
+            return [
+                'product' => $item->product,
+                'quantity' => $item->quantity,
+            ];
+        })->toArray();
+    }
+}
