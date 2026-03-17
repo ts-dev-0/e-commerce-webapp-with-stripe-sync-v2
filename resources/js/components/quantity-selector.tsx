b1a607c65098eea4026ec2c4ed8828e23b3bffc0
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-17 21:26:33 +0900

    feat: add logic to update quantity

--- resources/js/components/quantity-selector.tsx
+++ resources/js/components/quantity-selector.tsx
@@ -1,26 +1,55 @@
+import { update } from '@/routes/cart';
+import { router } from '@inertiajs/react';
+import { useState } from 'react';
 import { Button } from './ui/button';
 
 interface QuantitySelectorProps {
+    productId: number;
     value: number;
-    onChange: (prev: number) => void;
     min?: number;
     max?: number;
 }
 
 export function QuantitySelector({
+    productId,
     value,
-    onChange,
     min = 1,
     max = 10,
 }: QuantitySelectorProps) {
+    const [quantity, setQuantity] = useState(value);
+
+    const updateQuantity = (newQuantity: number) => {
+        setQuantity(newQuantity);
+
+        router.put(
+            // TODO: use cart_id
+            update({ cart: 1 }).url,
+            {
+                product_id: productId,
+                quantity: newQuantity,
+            },
+            {
+                preserveScroll: true,
+                preserveState: true,
+                onSuccess: (page) => {
+                    console.log('通信成功:', page);
+                },
+                onError: (errors) => {
+                    console.error('バックエンドのエラー:', errors);
+                    setQuantity(quantity);
+                },
+            },
+        );
+    };
+
     const handleMinus = () => {
-        if (value <= min) return;
-        onChange(value - 1);
+        if (quantity <= min) return;
+        updateQuantity(quantity - 1);
     };
 
     const handlePlus = () => {
-        if (value >= max) return;
-        onChange(value + 1);
+        if (quantity >= max) return;
+        updateQuantity(quantity + 1);
     };
 
     return (
@@ -31,18 +60,18 @@ export function QuantitySelector({
                 size="icon"
                 className="h-7 w-7 p-0"
                 aria-label="数量を減らす"
-                onClick={() => handleMinus()}
+                onClick={handleMinus}
             >
                 -
             </Button>
-            <span className="text-xs text-slate-700">{value}</span>
+            <span className="text-xs text-slate-700">{quantity}</span>
             <Button
                 type="button"
                 variant="outline"
                 size="icon"
                 className="h-7 w-7 p-0"
                 aria-label="数量を増やす"
-                onClick={() => handlePlus()}
+                onClick={handlePlus}
             >
                 +
             </Button>
