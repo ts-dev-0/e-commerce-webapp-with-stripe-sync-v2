7e3930567bd4132567562955573009b9d5f753e4
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-20 08:28:16 +0900

    style: fix background color in the auth layout

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function AuthLayout({ title, description, children }: Props) {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center gap-y-8 bg-slate-100">
+        <div className="flex min-h-svh flex-col items-center justify-center gap-y-8 bg-slate-50">
             <div className="mx-auto flex w-full max-w-11/12 flex-col space-y-5 pt-20 text-center">
                 <h1 className="text-xl font-medium">{title}</h1>
                 <p className="text-sm">{description}</p>
