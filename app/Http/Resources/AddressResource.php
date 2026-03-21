52f4c53e4c1112eaeb5a4c77560898b7d77ff3a1
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-21 20:49:43 +0900

    feat: add AddressResource

---
+++ app/Http/Resources/AddressResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AddressResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'fullName' => $this->full_name,
+            'postalCode' => $this->postalCode,
+            'prefecture' => $this->prefecture,
+            'city' => $this->city,
+            'addressLine' => $this->address_line,
+            'isDefault' => $this->is_default,
+        ];
+    }
+}
