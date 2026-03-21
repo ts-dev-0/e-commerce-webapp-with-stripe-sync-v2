8c8d55deeb88a91cad91b2f8724ce03997c30745
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-21 20:32:38 +0900

    feat: add defaultAddress method to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -85,6 +86,12 @@ class User extends Authenticatable
     public function isAdmin(): bool
     {
         return $this->is_admin;
+    }
+
+    public function defaultAddress(): HasMany
+    {
+        return $this->addresses()
+                ->where('is_default', true);
     }
 
     public function clearDefaultAddress(): void
