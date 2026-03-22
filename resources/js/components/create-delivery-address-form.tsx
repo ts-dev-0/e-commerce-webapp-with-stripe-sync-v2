453a14a4c434bec00d4a0b1d876d61bece84d0bc
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-22 21:22:22 +0900

    ref: componentize create delivery address form

---
+++ resources/js/components/create-delivery-address-form.tsx
@@ -0,0 +1,150 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import { PREFECTURES } from '@/const/prefectures';
+import { useAddress } from '@/hooks/use-address';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+
+interface CreateDeliveryAddressFormProps {
+    processing: boolean;
+}
+
+export default function CreateDeliveryAddressForm({
+    processing,
+}: CreateDeliveryAddressFormProps) {
+    const {
+        address,
+        handleCreateAddress,
+        handleFullName,
+        handlePostalCode,
+        handlePrefecture,
+        handleCity,
+        handleAddressLine,
+        handlePhoneNumber,
+        handleIsDefault,
+    } = useAddress();
+
+    return (
+        <>
+            <div className="mt-4 grid gap-4 sm:grid-cols-2">
+                <div>
+                    <label className="text-xs font-medium text-slate-600">
+                        お名前
+                    </label>
+                    <Input
+                        value={address['fullName']}
+                        placeholder="山田 太郎"
+                        onChange={(e) => handleFullName(e.target.value)}
+                    />
+                </div>
+
+                <div className="col-span-2 flex flex-col gap-y-4">
+                    <div>
+                        <label className="text-xs font-medium text-slate-600">
+                            郵便番号(半角数字・ハイフンなし)
+                        </label>
+                        <div className="flex items-center gap-x-2">
+                            <Input
+                                value={address['postalCode']}
+                                placeholder="0000000"
+                                className="w-fit"
+                                maxLength={7}
+                                onChange={(e) =>
+                                    handlePostalCode(e.target.value)
+                                }
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-y-4">
+                        <div>
+                            <label className="text-xs font-medium text-slate-600">
+                                都道府県
+                            </label>
+                            <Select
+                                onValueChange={(e) => handlePrefecture(e)}
+                                value={address['prefecture']}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue placeholder="都道府県を選択" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        {PREFECTURES.map((prefecture) => (
+                                            <SelectItem
+                                                key={prefecture.value}
+                                                value={prefecture.value}
+                                            >
+                                                {prefecture.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <label className="text-xs font-medium text-slate-600">
+                                市区町村
+                            </label>
+                            <Input
+                                value={address['city']}
+                                placeholder="○○区"
+                                onChange={(e) => handleCity(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <label className="text-xs font-medium text-slate-600">
+                                丁目・番地・号 建物名／会社名・部屋番号
+                                （数字は半角数字）
+                            </label>
+                            <Input
+                                value={address['addressLine']}
+                                placeholder="○○町1-2-3 ○○ビル 101号室"
+                                onChange={(e) =>
+                                    handleAddressLine(e.target.value)
+                                }
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <label className="text-xs font-medium text-slate-600">
+                        電話番号
+                    </label>
+                    <Input
+                        value={address['phoneNumber']}
+                        placeholder="09012345678"
+                        maxLength={11}
+                        onChange={(e) => handlePhoneNumber(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="mt-4 flex items-center justify-start gap-2">
+                <Input
+                    checked={address['isDefault']}
+                    type="checkbox"
+                    className="h-4 w-4 border-slate-300 text-emerald-600 accent-emerald-600 focus:ring-emerald-500"
+                    onChange={(e) => handleIsDefault(e.target.checked)}
+                />
+                <Label className="text-sm font-medium text-slate-700">
+                    いつもこの住所に届ける
+                </Label>
+            </div>
+            <Button
+                className="mt-6 w-full rounded-md bg-emerald-600 px-4 py-2 text-sm text-white hover:bg-emerald-700"
+                variant="default"
+                disabled={processing}
+                onClick={handleCreateAddress}
+            >
+                この住所を使用
+            </Button>
+        </>
+    );
+}
