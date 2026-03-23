16cab0238b2618a0fb424e2327150d4661027bb9
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-23 20:57:49 +0900

    Refactor Order action test class

--- tests/Feature/Actions/User/Order/ViewOrderHistoryTest.php
+++ tests/Feature/Actions/User/Order/ViewOrderHistoryTest.php
@@ -2,39 +2,40 @@
 
 namespace Tests\Feature\Actions\User\Order;
 
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Actions\User\Order\ViewOrderHistory;
 use App\Models\Order;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
 class ViewOrderHistoryTest extends TestCase
 {
     use RefreshDatabase;
 
     private ViewOrderHistory $action;
+    private User $user;
 
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->action = new ViewOrderHistory();
+        $this->user = User::factory()->create();
     }
 
     public function test_user_can_view_their_order_history()
     {
-        $user = User::factory()->create();
-        $otherUser = User::factory()->create();
+       $otherUser = User::factory()->create();
 
         $userOrders = Order::factory()->count(2)->create([
-            'user_id' => $user->id,
+            'user_id' => $this->user->id,
         ]);
 
         Order::factory()->create([
             'user_id' => $otherUser->id,
         ]);
 
-        $orders = $this->action->handle($user);
+        $orders = $this->action->handle($this->user);
 
         $this->assertCount(2, $orders);
 
@@ -49,19 +50,17 @@ class ViewOrderHistoryTest extends TestCase
 
     public function test_orders_are_returned_in_descending_created_at_order()
     {
-        $user = User::factory()->create();
-
         $oldOrder = Order::factory()->create([
-            'user_id' => $user->id,
+            'user_id' => $this->user->id,
             'created_at' => now()->subDays(2),
         ]);
 
         $newOrder = Order::factory()->create([
-            'user_id' => $user->id,
+            'user_id' => $this->user->id,
             'created_at' => now(),
         ]);
 
-        $orders = $this->action->handle($user)
+        $orders = $this->action->handle($this->user)
             ->values();
 
         $this->assertCount(2, $orders);
@@ -72,9 +71,7 @@ class ViewOrderHistoryTest extends TestCase
 
     public function test_returns_empty_collection_when_user_has_no_orders()
     {
-        $user = User::factory()->create();
-
-        $orders = $this->action->handle($user);
+        $orders = $this->action->handle($this->user);
 
         $this->assertCount(0, $orders);
         $this->assertTrue($orders->isEmpty());
