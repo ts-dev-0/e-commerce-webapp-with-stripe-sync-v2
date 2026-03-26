4dbaee8d811856aa51837d2490010e59d75a0559
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-26 17:55:22 +0900

    feat: add set default address route

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('search/products', SearchPublishedProductController::class)->name('search.products');
 
     Route::post('addresses', [AddressController::class, 'store'])->name('addresses.store');
+    Route::patch('addresses/{address}/default', [AddressController::class, 'updateDefault']
+        )->name('addresses.default.update');
 });
 
 require __DIR__.'/settings.php';
