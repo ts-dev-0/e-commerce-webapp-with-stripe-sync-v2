38c00ec3e288acd88e142c945cfa5efe344f653f
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-31 12:48:57 +0900

    feat: add getProductDetail action class and the corresponding tests

---
+++ app/Actions/ProductDetail/getProductDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\ProductDetail;
+
+use App\Models\Product;
+
+class getProductDetail
+{
+  public function handle(Product $product): array
+  {
+    $reviews = $product->reviews;
+    $averageReview = $reviews->avg('rating') ?? 0.0;
+
+    return [
+      'reviews' => $reviews,
+      'averageReview' => $averageReview,
+    ];
+  }
+}
