403187afd7fc48453821d060cb7e8f51c970a3f7
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-03-31 18:45:16 +0900

    feat: add GetProductDetail action class and reviews, averageRating data props

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ProductDetail\GetProductDetail;
 use App\Http\Resources\ProductResource;
+use App\Http\Resources\ReviewResource;
 use App\Models\Product;
 
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    public function show(Product $product)
+    public function show(Product $product, GetProductDetail $action)
     {
+        $result = $action->handle($product);
+
         return Inertia::render('product/show', [
             'data' => ProductResource::make($product),
+            'reviews' => ReviewResource::collection($result['reviews']),
+            'averageRating' => $result['averageRating'],
         ]);
     }
 }
