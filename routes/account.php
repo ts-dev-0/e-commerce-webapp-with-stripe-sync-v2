9375d420d2a2b7f7e5ead4ce9d0a8387b1fcd2f5
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-03 17:58:42 +0900

    feat: add account/order, account/addresses route

--- routes/account.php
+++ routes/account.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-  Route::redirect('account', 'account/orders');
+Route::middleware(['auth', 'verified'])->prefix('account')->group(function () {
+  Route::redirect('/', 'account/orders');
 
-  Route::get('account/orders', [OrderController::class, 'index'])->name('account.orders');
-  Route::get('account/addresses', [AddressController::class, 'index'])->name('account.addresses');
+  Route::get('orders', [OrderController::class, 'index'])->name('account.orders');
+  Route::get('addresses', [AddressController::class, 'index'])->name('account.addresses');
 });
