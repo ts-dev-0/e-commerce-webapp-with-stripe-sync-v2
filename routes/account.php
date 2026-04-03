e973e6c9357bf05f27363bbff2b7351c18ca5d95
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-03 18:09:21 +0900

    ref: Move account/profile to account.tsx

--- routes/account.php
+++ routes/account.php
@@ -2,11 +2,15 @@
 
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('account')->group(function () {
-  Route::redirect('/', 'account/orders');
+  Route::redirect('/', '/account/profile');
 
   Route::get('orders', [OrderController::class, 'index'])->name('account.orders');
   Route::get('addresses', [AddressController::class, 'index'])->name('account.addresses');
+
+  Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
 });
--- routes/settings.php
+++ routes/settings.php
@@ -2,16 +2,7 @@
 
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
-use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::middleware(['auth'])->prefix('account')->group(function () {
-    Route::redirect('settings', 'settings/password');
-
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-});
 
 Route::middleware(['auth', 'verified'])->prefix('account')->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
