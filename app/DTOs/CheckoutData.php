284c261ca49a85f4bdd8b3426171dc605b057886
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-08 17:54:20 +0900

    fix: CheckoutDTO class

--- app/DTOs/CheckoutData.php
+++ app/DTOs/CheckoutData.php
@@ -2,20 +2,22 @@
 
 namespace App\DTOs;
 
-use Illuminate\Support\Collection;
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Collection;
 
 readonly class CheckoutData
 {
     /**
      * @param Collection<int, \App\Models\CartItem> $cartItems
-     * @param Collection<int, \App\Models\Address> $addresses
+     * @param Collection<int, \App\Models\Address> $anotherAddresses
      */
     public function __construct(
         public Collection $cartItems,
-        public int $subtotal,
+        public ?Address $defaultAddress,
+        public Collection $anotherAddresses,
         public array $deliveryDate,
-        public Collection $addresses,
         public int $shippingFee,
+        public int $subtotal,
         public int $total,
     ) {}
 }
