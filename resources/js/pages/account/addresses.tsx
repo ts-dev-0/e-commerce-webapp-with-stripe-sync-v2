9bb574bfd8589f5dacbd5795e9134c156065242c
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-09 21:20:19 +0900

    style: Add set default button to addresses page

--- resources/js/pages/account/addresses.tsx
+++ resources/js/pages/account/addresses.tsx
@@ -71,30 +71,42 @@ export default function Addresses({ data }: AddressesProps) {
                                 </p>
                             </div>
 
-                            <div className="mt-4 flex justify-end gap-2 border-t border-slate-200 pt-4">
-                                <Button
-                                    className="bg-emerald-600 hover:bg-emerald-700"
-                                    onClick={() =>
-                                        openModal(
-                                            'editDeliveryAddress',
-                                            address,
-                                        )
-                                    }
-                                >
-                                    編集
-                                </Button>
+                            <div className="mt-4 flex items-center justify-between gap-2 border-t border-slate-200 pt-4">
+                                <div>
+                                    {!address.isDefault && (
+                                        <Button
+                                            variant={'link'}
+                                            className="cursor-pointer"
+                                        >
+                                            既定の住所に設定
+                                        </Button>
+                                    )}
+                                </div>
+                                <div className="flex gap-x-2">
+                                    <Button
+                                        className="bg-emerald-600 hover:bg-emerald-700"
+                                        onClick={() =>
+                                            openModal(
+                                                'editDeliveryAddress',
+                                                address,
+                                            )
+                                        }
+                                    >
+                                        編集
+                                    </Button>
 
-                                <Button
-                                    type="button"
-                                    variant="destructive"
-                                    onClick={() =>
-                                        openModal('deleteDeliveryAddress', {
-                                            id: address.id,
-                                        })
-                                    }
-                                >
-                                    削除
-                                </Button>
+                                    <Button
+                                        type="button"
+                                        variant="destructive"
+                                        onClick={() =>
+                                            openModal('deleteDeliveryAddress', {
+                                                id: address.id,
+                                            })
+                                        }
+                                    >
+                                        削除
+                                    </Button>
+                                </div>
                             </div>
                         </div>
                     ))}
