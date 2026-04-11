3c30642120d5554ba259afaf762216231f7286a2
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-11 10:19:12 +0900

    feat: Add GetAvailableOrderYears action class and the coresspondign tests

---
+++ tests/Feature/Order/GetAvailableOrderYearsTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Order;
+
+use App\Actions\Order\GetAvailableOrderYears;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class GetAvailableOrderYearsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private GetAvailableOrderYears $action;
+    private User $user2020;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->action = new GetAvailableOrderYears();
+        $this->user2020 = User::factory()->create([
+            'created_at' => Carbon::create(2020, 4, 11),
+        ]);
+    }
+
+    public function test_it_returns_available_order_years_data()
+    {
+        $timeFilter = [
+            [
+                'label' => '2026',
+                'value' => 'year-2026',
+            ],
+            [
+                'label' => '2025',
+                'value' => 'year-2025',
+            ],
+            [
+                'label' => '2024',
+                'value' => 'year-2024',
+            ],
+            [
+                'label' => '2023',
+                'value' => 'year-2023',
+            ],
+            [
+                'label' => '2022',
+                'value' => 'year-2022',
+            ],
+            [
+                'label' => '2021',
+                'value' => 'year-2021',
+            ],
+            [
+                'label' => '2020',
+                'value' => 'year-2020',
+            ],
+        ];
+
+        $result = $this->action->handle($this->user2020);
+
+        $this->assertEquals($timeFilter, $result);
+    }
+}
