f41b013c81bc172f73d4cc529857e9071ce9fe74
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-14 15:23:09 +0900

    feat: Add logic of update and setDefault

--- app/Policies/AddressPolicy.php
+++ app/Policies/AddressPolicy.php
@@ -13,7 +13,15 @@ class AddressPolicy
      */
     public function update(User $user, Address $address): bool
     {
-        return false;
+        return $user->id === $address->user_id;
+    }
+
+    /**
+     * Determine whether the user can set the address as their default address.
+     */
+    public function setDefault(User $user, Address $address): bool
+    {
+        return $user->id === $address->user_id;
     }
 
     /**
