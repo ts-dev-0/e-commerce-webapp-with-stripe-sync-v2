a9361f8a8ca4d9ea4b3048bbb4d4e1051534a795
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-15 21:48:23 +0900

    fix: Chagen from route in PasswordUpdateTest

--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -11,24 +11,13 @@ class PasswordUpdateTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_password_update_page_is_displayed()
-    {
-        $user = User::factory()->create();
-
-        $response = $this
-            ->actingAs($user)
-            ->get(route('user-password.edit'));
-
-        $response->assertOk();
-    }
-
     public function test_password_can_be_updated()
     {
         $user = User::factory()->create();
 
         $response = $this
             ->actingAs($user)
-            ->from(route('user-password.edit'))
+            ->from(route('account.security'))
             ->put(route('user-password.update'), [
                 'current_password' => 'password',
                 'password' => 'new-password',
@@ -37,7 +26,7 @@ class PasswordUpdateTest extends TestCase
 
         $response
             ->assertSessionHasNoErrors()
-            ->assertRedirect(route('user-password.edit'));
+            ->assertRedirect(route('account.security'));
 
         $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
@@ -48,7 +37,7 @@ class PasswordUpdateTest extends TestCase
 
         $response = $this
             ->actingAs($user)
-            ->from(route('user-password.edit'))
+            ->from(route('account.security'))
             ->put(route('user-password.update'), [
                 'current_password' => 'wrong-password',
                 'password' => 'new-password',
@@ -57,6 +46,6 @@ class PasswordUpdateTest extends TestCase
 
         $response
             ->assertSessionHasErrors('current_password')
-            ->assertRedirect(route('user-password.edit'));
+            ->assertRedirect(route('account.security'));
     }
 }
