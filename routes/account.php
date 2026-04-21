87019b4008b19def890f7164d0f42d01256888d1
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-21 21:25:34 +0900

    fix: Formatted account.php

--- routes/account.php
+++ routes/account.php
@@ -8,18 +8,28 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->prefix('account')->group(function () {
-  Route::get('/', function() {
+  Route::get('/', function () {
     return Inertia::render('account/index');
   })->name('account.index');
 
-  Route::get('orders', [OrderController::class, 'index'])->name('account.orders');
+  Route::get('orders', [OrderController::class, 'index'])
+    ->name('account.orders');
+
   Route::post('orders/{order}/cancel', [OrderController::class, 'cancel'])
-        ->name('orders.cancel');
-  Route::get('addresses', [AddressController::class, 'index'])->name('account.addresses');
+    ->name('orders.cancel');
 
-  Route::get('security', [SecurityController::class, 'index'])->name('account.security');
+  Route::get('addresses', [AddressController::class, 'index'])
+    ->name('account.addresses');
 
-  Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('security', [SecurityController::class, 'index'])
+    ->name('account.security');
+
+  Route::get('profile', [ProfileController::class, 'edit'])
+    ->name('profile.edit');
+
+  Route::patch('profile', [ProfileController::class, 'update'])
+    ->name('profile.update');
+
+  Route::delete('settings/profile', [ProfileController::class, 'destroy'])
+    ->name('profile.destroy');
 });
