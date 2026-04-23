ed85d170f03f0f0dd8499a936b2971d4f72525be
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-23 21:56:05 +0900

    fix: Add primary variant to Badge component

--- resources/js/components/ui/badge.tsx
+++ resources/js/components/ui/badge.tsx
@@ -17,6 +17,8 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        primary:
+          "border-transparent bg-indigo-500 [a&]:hover:bg-indigo-700 text-white",
       },
     },
     defaultVariants: {
