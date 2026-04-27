9df80f9e63ba108432c5acdeec25bf52ac3f4cbe
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-27 16:46:04 +0900

    feat: Add decrement stock when order successed

--- app/Actions/Checkout/ProcessCheckout.php
+++ app/Actions/Checkout/ProcessCheckout.php
@@ -7,44 +7,48 @@ use App\Models\Order;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Stripe\Checkout\Session;
-use Stripe\Stripe;
 
 class ProcessCheckout
 {
     public function handle(User $user, string $sessionId)
     {
         $session = Session::retrieve($sessionId);
+
         $addressId = $session->metadata->address_id;
-        $delivaryAddress = Address::find($addressId);
+        $deliveryAddress = Address::findOrFail($addressId);
         $totalAmount = $session->amount_total;
         $orderNumber = 'ORD-' . strtoupper(Str::random(8));
 
         $order = Order::create([
-            'user_id'      => $user->id,
+            'user_id' => $user->id,
             'order_number' => $orderNumber,
             'total_amount' => $totalAmount,
-            'full_name' => $delivaryAddress->full_name,
-            'postal_code' => $delivaryAddress->postal_code,
-            'prefecture' => $delivaryAddress->prefecture,
-            'city' => $delivaryAddress->city,
-            'address_line' => $delivaryAddress->address_line,
-            'phone_number' => $delivaryAddress->phone_number,
+            'full_name' => $deliveryAddress->full_name,
+            'postal_code' => $deliveryAddress->postal_code,
+            'prefecture' => $deliveryAddress->prefecture,
+            'city' => $deliveryAddress->city,
+            'address_line' => $deliveryAddress->address_line,
+            'phone_number' => $deliveryAddress->phone_number,
             'ordered_at' => now(),
         ]);
 
         $cart = $user->currentCart();
         $cartItems = $cart->products()->get();
+
         foreach ($cartItems as $product) {
             $quantity = $product->pivot->quantity;
-            $price    = $product->price;
+
+            $price = $product->price;
 
             $order->items()->create([
-                'product_id'   => $product->id,
+                'product_id' => $product->id,
                 'product_name' => $product->name,
-                'quantity'     => $quantity,
-                'price'        => $price,
-                'subtotal'     => $price * $quantity,
+                'quantity' => $quantity,
+                'price' => $price,
+                'subtotal' => $price * $quantity,
             ]);
+
+            $product->decrement('stock', $quantity);
         }
 
         $cart->products()->detach();
