b182dffc1b1e74da44cedf9dc930c7e54b53c1fa
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-04-29 15:19:14 +0900

    fix: Change logic code in CreateCheckoutSession class

--- app/Actions/Stripe/CreateCheckoutSession.php
+++ app/Actions/Stripe/CreateCheckoutSession.php
@@ -3,40 +3,32 @@
 namespace App\Actions\Stripe;
 
 use App\Models\User;
+use App\Models\Product;
 use Stripe\Checkout\Session;
 use Stripe\Stripe;
+use Illuminate\Support\Collection;
 
 class CreateCheckoutSession
 {
+    public function __construct()
+    {
+        Stripe::setApiKey(config('services.stripe.secret'));
+    }
+
+    /**
+     * @throws \DomainException
+     */
     public function handle(User $user, int $addressId): string
     {
-        $cart = $user->currentCart();
-        $cartItems = $cart->products()->get();
+        $cartItems = $user->currentCart()->products()->get();
 
         if ($cartItems->isEmpty()) {
             throw new \DomainException('Cart is empty.');
         }
 
-        Stripe::setApiKey(config('services.stripe.secret'));
-
-        $lineItems = [];
-
-        foreach ($cartItems as $product) {
-            $lineItems[] = [
-                'price_data' => [
-                    'currency' => 'jpy',
-                    'product_data' => [
-                        'name' => $product->name,
-                    ],
-                    'unit_amount' => $product->price,
-                ],
-                'quantity' => $product->pivot->quantity,
-            ];
-        }
-
         $session = Session::create([
             'payment_method_types' => ['card'],
-            'line_items' => $lineItems,
+            'line_items' => $this->mapCartItemsToStripeFormat($cartItems),
             'mode' => 'payment',
             'success_url' => route('checkout.success') . '?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => route('checkout.failed'),
@@ -47,5 +39,19 @@ class CreateCheckoutSession
         ]);
 
         return $session->url;
+    }
+
+    private function mapCartItemsToStripeFormat(Collection $items): array
+    {
+        return $items->map(fn(Product $product) => [
+            'price_data' => [
+                'currency' => 'jpy',
+                'product_data' => [
+                    'name' => $product->name,
+                ],
+                'unit_amount' => $product->price,
+            ],
+            'quantity' => $product->pivot->quantity,
+        ])->toArray();
     }
 }
