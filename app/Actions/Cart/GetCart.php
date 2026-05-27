b10ac478a09c7179e83a69d58650c9e6584cc61b
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-05-27 16:12:31 +0900

    ref: Change the  logic to get user's cart item data

--- app/Actions/Cart/GetCart.php
+++ app/Actions/Cart/GetCart.php
@@ -3,19 +3,13 @@
 namespace App\Actions\Cart;
 
 use App\DTOs\CartData;
-use App\Models\CartItem;
 use App\Models\User;
-
 
 class GetCart
 {
     public function handle(User $user): CartData
     {
-        $cartItems = CartItem::with('product')
-            ->whereHas('cart', function ($query) use ($user) {
-                $query->where('user_id', $user->id);
-            })
-            ->get();
+        $cartItems = $user->currentCart()->items()->get();
 
         $subtotal = $cartItems->sum(function ($item) {
             return $item->product->price * $item->quantity;
