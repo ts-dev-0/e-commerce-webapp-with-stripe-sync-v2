264ad7c1c55bd8db1bfeb8b1b00648b94a4791f5
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-05-29 20:20:02 +0900

    ref: Add CreateOrder action class

---
+++ app/Actions/Order/CreateOrder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Order;
+
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\User;
+use Stripe\Checkout\Session;
+
+class CreateOrder
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(
+        User $user,
+        Session $session,
+        Address $address
+    ): Order {
+        return Order::create([
+            'user_id' => $user->id,
+            'order_number' => 'ORD-' . strtoupper(Str::random(8)),
+            'total_amount' => $session->amount_total,
+            'full_name' => $address->full_name,
+            'postal_code' => $address->postal_code,
+            'prefecture' => $address->prefecture,
+            'city' => $address->city,
+            'address_line' => $address->address_line,
+            'phone_number' => $address->phone_number,
+            'ordered_at' => now(),
+        ]);
+    }
+}
