43599b4ad6b1bcde9270a22f2b7a3f1bfbdc8895
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-05-29 20:24:52 +0900

    ref: Add CreateOrderItem action class

---
+++ app/Actions/OrderItem/CreateOrderItem.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\OrderItem;
+
+use App\Models\Order;
+use App\Models\User;
+
+class CreateOrderItem
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(User $user, Order $order): void
+    {
+        $cart = $user->currentCart();
+        $products = $cart->products()->get();
+
+        foreach ($products as $product) {
+            $quantity = $product->pivot->quantity;
+            $price = $product->price;
+
+            $order->items()->create([
+                'product_id' => $product->id,
+                'product_name' => $product->name,
+                'quantity' => $quantity,
+                'price' => $price,
+                'subtotal' => $price * $quantity,
+            ]);
+
+            $product->decrement('stock', $quantity, []);
+        }
+    }
+}
