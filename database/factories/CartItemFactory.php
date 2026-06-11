0f4f13e8cf7557aea70455742c6838a92e6b413e
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-11 22:08:54 +0900

    feat: Add 'product_id' to CartItemFactory definition method

--- database/factories/CartItemFactory.php
+++ database/factories/CartItemFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,6 +18,7 @@ class CartItemFactory extends Factory
     public function definition(): array
     {
         return [
+            'product_id' => Product::factory(),
             'quantity' => $this->faker->numberBetween(1, 3),
         ];
     }
