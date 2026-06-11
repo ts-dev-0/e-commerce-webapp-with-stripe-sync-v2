9d128dedbddeab2341bc49b6d5572b64527429e5
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-11 22:07:43 +0900

    fix: PHPDoc in CartItemFactory

--- database/factories/CartItemFactory.php
+++ database/factories/CartItemFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\=CartItem>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CartItem>
  */
 class CartItemFactory extends Factory
 {
