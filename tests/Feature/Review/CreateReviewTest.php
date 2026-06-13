42cce1aeb3781cb907eb1f58d815881761643fb8
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-13 14:51:57 +0900

    ref: CreateReview class

--- tests/Feature/Review/CreateReviewTest.php
+++ tests/Feature/Review/CreateReviewTest.php
@@ -40,6 +40,26 @@ class CreateReviewTest extends TestCase
     /**
      *  Exception Cases
      */
+    public function test_it_throws_review_already_exists_exception()
+    {
+        $product = Product::factory()->create();
+
+        $user = User::factory()->create();
+        Review::factory()->create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'rating' => 5,
+            'comment' => 'Good Product!',
+        ]);
+        $reviewData = [
+            'product_id' => $product->id,
+            'rating' => 1,
+            'comment' => 'Bad Product',
+        ];
+
+        $this->expectException(\App\Exceptions\ReviewAlreadyExistsException::class);
+        app(CreateReview::class)->handle($user, $reviewData);
+    }
 
     /**
      *  Edge Cases
