f33a14dd735bb142a52e95731948f36b3477569a
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-17 21:15:31 +0900

    ref: SetDefaultAddress action class

--- tests/Feature/Address/SetDefaultAddressTest.php
+++ tests/Feature/Address/SetDefaultAddressTest.php
@@ -12,60 +12,57 @@ class SetDefaultAddressTest extends TestCase
 {
     use RefreshDatabase;
 
-    private SetDefaultAddress $action;
-    private User $user;
-
-    protected function setUp(): void
+    /**
+     *  Happy Path
+     */
+    public function test_it_switches_the_default_address()
     {
-        parent::setUp();
-
-        $this->action = new SetDefaultAddress();
-        $this->user = User::factory()->create();
-    }
-
-    public function test_action_sets_given_address_as_default()
-    {
-        /** @var \App\Models\Address $defaultAddress */
-        $defaultAddress = Address::factory()->create([
+        $user = User::factory()->create();
+        $oldDefaultAddress = Address::factory()->create([
+            'user_id' => $user->id,
             'is_default' => true,
-            'user_id' => $this->user->id,
         ]);
-
-        /** @var \App\Models\Address $newDefaultAddress */
         $newDefaultAddress = Address::factory()->create([
-            'user_id' => $this->user->id,
-        ]);
-
-        $this->action->handle($this->user, $newDefaultAddress);
-
-        $this->assertDatabaseHas('addresses', [
-            'id' => $defaultAddress->id,
+            'user_id' => $user->id,
             'is_default' => false,
         ]);
 
+        app(SetDefaultAddress::class)->handle($user, $newDefaultAddress);
+
+        $this->assertDatabaseHas('addresses', [
+            'id' => $oldDefaultAddress->id,
+            'user_id' => $user->id,
+            'is_default' => false,
+        ]);
         $this->assertDatabaseHas('addresses', [
             'id' => $newDefaultAddress->id,
+            'user_id' => $user->id,
             'is_default' => true,
         ]);
     }
 
-    public function test_action_does_not_update_when_address_is_already_default()
+    /**
+     *  Exception Cases
+     */
+
+    /**
+     *  Edge Cases
+     */
+    public function test_it_keeps_the_address_as_default_when_it_is_already_the_default_address()
     {
-        /** @var \App\Models\Address $defaultAddress */
+        $user = User::factory()->create();
         $defaultAddress = Address::factory()->create([
+            'user_id' => $user->id,
             'is_default' => true,
-            'user_id' => $this->user->id,
         ]);
 
-        $originalUpdatedAt = $defaultAddress->updated_at;
 
-        $this->action->handle($this->user, $defaultAddress);
+        app(SetDefaultAddress::class)->handle($user, $defaultAddress);
 
-        $defaultAddress->refresh();
-
-        $this->assertEquals(
-            $originalUpdatedAt,
-            $defaultAddress->updated_at
-        );
+        $this->assertDatabaseHas('addresses', [
+            'id' => $defaultAddress->id,
+            'user_id' => $user->id,
+            'is_default' => true,
+        ]);
     }
 }
