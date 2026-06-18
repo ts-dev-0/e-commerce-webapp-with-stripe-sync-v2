861efa7ee7ef1b82e54a0366dc01edfef393c2d2
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-18 18:05:02 +0900

    ref: Rename SearchPublishedProductRequest class

--- app/Http/Controllers/SearchProductsController.php
+++ app/Http/Controllers/SearchProductsController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Actions\Product\SearchProducts;
 use Inertia\Inertia;
-use App\Http\Requests\Search\SearchPublishedProductRequest;
+use App\Http\Requests\Search\SearchProductsRequest;
 use App\Http\Resources\ProductResource;
 
 class SearchProductsController extends Controller
 {
-    public function __invoke(SearchPublishedProductRequest $request, SearchProducts $action)
+    public function __invoke(SearchProductsRequest $request, SearchProducts $action)
     {
         $validatedData = $request->validated();
         $products = $action->handle($validatedData['keyword']);
