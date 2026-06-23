f3ff358a3835a3c8acb8867b8b19dca044c274e6
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-06-23 16:41:44 +0900

    ref: Update edit delivery address form UI

--- resources/js/components/edit-delivery-address-form.tsx
+++ resources/js/components/edit-delivery-address-form.tsx
@@ -11,6 +11,7 @@ import { update } from '@/routes/addresses';
 import { useModalStore } from '@/stores/modalStore';
 import { Address, UpdateAddress } from '@/types/address';
 import { useForm } from '@inertiajs/react';
+import React from 'react';
 import ErrorMessage from './error-message';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
@@ -60,35 +61,21 @@ export default function EditDeliveryAddressForm({
     return (
         <>
             <div className="mt-4 grid gap-4 sm:grid-cols-2">
-                <div className="flex flex-col gap-y-2">
-                    <label className="text-xs font-medium text-slate-600">
-                        お名前
-                    </label>
-                    <Input
-                        value={data.fullName}
-                        placeholder="山田 太郎"
-                        onChange={(e) => setData('fullName', e.target.value)}
-                    />
-                    <ErrorMessage message={errors.fullName} />
-                </div>
-
+                <FormInputField
+                    label="お名前"
+                    value={data.fullName}
+                    placeholder="山田 太郎"
+                    onChange={(e) => setData('fullName', e.target.value)}
+                    errorMessage={errors.fullName}
+                />
                 <div className="col-span-2 flex flex-col gap-y-4">
-                    <div className="flex flex-col gap-y-2">
-                        <label className="text-xs font-medium text-slate-600">
-                            郵便番号(半角数字・ハイフンなし)
-                        </label>
-
-                        <Input
-                            value={data.postalCode}
-                            placeholder="0000000"
-                            className="w-fit"
-                            maxLength={7}
-                            onChange={(e) =>
-                                setData('postalCode', e.target.value)
-                            }
-                        />
-                        <ErrorMessage message={errors.postalCode} />
-                    </div>
+                    <FormInputField
+                        label="郵便番号(半角数字・ハイフンなし)"
+                        value={data.postalCode}
+                        placeholder="1234567"
+                        onChange={(e) => setData('postalCode', e.target.value)}
+                        errorMessage={errors.postalCode}
+                    />
                     <div className="flex flex-col gap-y-4">
                         <div className="flex flex-col gap-y-2">
                             <label className="text-xs font-medium text-slate-600">
@@ -118,48 +105,34 @@ export default function EditDeliveryAddressForm({
                             </Select>
                             <ErrorMessage message={errors.prefecture} />
                         </div>
-                        <div className="flex flex-col gap-y-2">
-                            <label className="text-xs font-medium text-slate-600">
-                                市区町村
-                            </label>
-                            <Input
-                                value={data.city}
-                                placeholder="○○区"
-                                onChange={(e) =>
-                                    setData('city', e.target.value)
-                                }
-                            />
-                            <ErrorMessage message={errors.city} />
-                        </div>
-                        <div className="flex flex-col gap-y-2">
-                            <label className="text-xs font-medium text-slate-600">
-                                丁目・番地・号 建物名／会社名・部屋番号
-                                （数字は半角数字）
-                            </label>
-                            <Input
-                                value={data.addressLine}
-                                placeholder="○○町1-2-3 ○○ビル 101号室"
-                                onChange={(e) =>
-                                    setData('addressLine', e.target.value)
-                                }
-                            />
-                            <ErrorMessage message={errors.addressLine} />
-                        </div>
+                        <FormInputField
+                            label="市区町村"
+                            value={data.city}
+                            placeholder="○○区"
+                            onChange={(e) => setData('city', e.target.value)}
+                            errorMessage={errors.city}
+                        />
+                        <FormInputField
+                            label="丁目・番地・号 建物名／会社名・部屋番号
+                                （数字は半角数字）"
+                            value={data.addressLine}
+                            placeholder="○○町1-2-3 ○○ビル 101号室"
+                            onChange={(e) =>
+                                setData('addressLine', e.target.value)
+                            }
+                            errorMessage={errors.addressLine}
+                        />
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-y-2">
-                    <label className="text-xs font-medium text-slate-600">
-                        電話番号
-                    </label>
-                    <Input
-                        value={data.phoneNumber}
-                        placeholder="09012345678"
-                        maxLength={11}
-                        onChange={(e) => setData('phoneNumber', e.target.value)}
-                    />
-                    <ErrorMessage message={errors.phoneNumber} />
-                </div>
+                <FormInputField
+                    label="電話番号"
+                    value={data.phoneNumber}
+                    placeholder="09012345678"
+                    maxLength={11}
+                    onChange={(e) => setData('phoneNumber', e.target.value)}
+                    errorMessage={errors.phoneNumber}
+                />
             </div>
             <Button
                 className="mt-6 w-full rounded-md bg-emerald-600 px-4 py-2 text-sm text-white hover:bg-emerald-700"
@@ -171,5 +144,28 @@ export default function EditDeliveryAddressForm({
                 変更を保存
             </Button>
         </>
+    );
+}
+
+interface FormInputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    label: string;
+    errorMessage?: string;
+}
+
+function FormInputField({
+    label,
+    errorMessage,
+    ...props
+}: FormInputFieldProps) {
+    return (
+        <div className="flex flex-col gap-y-2">
+            <label className="text-xs font-medium text-slate-600">
+                {label}
+            </label>
+
+            <Input {...props} />
+
+            <ErrorMessage message={errorMessage} />
+        </div>
     );
 }
