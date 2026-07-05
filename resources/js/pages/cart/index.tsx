478ed6a921b5668c3ed6ebb5fa68bd505013d2cd
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-07-05 13:27:38 +0900

    ref: Rename Cart page name

--- resources/js/pages/cart/index.tsx
+++ resources/js/pages/cart/index.tsx
@@ -1,6 +1,7 @@
-import CartItemCard from '@/components/cart-item-card';
+import { EmptyState } from '@/components/empty-state';
 import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
+import CartItemCard from '@/pages/cart/component/cart-item-card';
 import { index } from '@/routes/checkout';
 import { Cart as CartType } from '@/types/cart';
 import { Head, Link } from '@inertiajs/react';
@@ -9,36 +10,32 @@ interface CartProps {
     cart: CartType;
 }
 
-export default function Cart({ cart }: CartProps) {
+export default function Index({ cart }: CartProps) {
     return (
         <AppLayout>
-            <Head title="Cart" />
+            <Head title="ショッピングカート" />
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="grid gap-6 lg:grid-cols-3">
                     <section className="lg:col-span-2">
-                        <div className="space-y-4">
-                            {cart.items.length <= 0 ? (
-                                <>
-                                    <h1 className="text-2xl font-bold text-slate-800">
-                                        カートは空です
-                                    </h1>
-                                    <p className="text-sm text-slate-600">
-                                        ショッピングカートをご利用ください。食料品、衣類、生活用品、電子機器などを入れましょう。{' '}
-                                    </p>
-                                </>
-                            ) : (
-                                cart.items.map(({ id, product, quantity }) => {
+                        {cart.items.length <= 0 ? (
+                            <EmptyState
+                                title="カートは空です"
+                                description="ショッピングカートをご利用ください。食料品、衣類、生活用品、電子機器などを入れましょう。"
+                            />
+                        ) : (
+                            <div className="flex flex-col gap-y-3">
+                                {cart.items.map(({ id, product, quantity }) => {
                                     return (
                                         <CartItemCard
                                             key={id}
                                             cartItemId={id}
                                             product={product}
-                                            initialQuantity={quantity}
+                                            quantity={quantity}
                                         />
                                     );
-                                })
-                            )}
-                        </div>
+                                })}
+                            </div>
+                        )}
                     </section>
 
                     <aside className="flex h-fit flex-col gap-6 rounded-lg border border-slate-200 bg-white p-4 shadow-sm">
