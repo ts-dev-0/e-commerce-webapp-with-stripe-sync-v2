01207f63efea7def6089ccfa1c3d7fd96c10c305
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-07-20 13:02:58 +0900

    fix: Remove deliveryData propaty from Checkout

--- resources/js/types/checkout.ts
+++ resources/js/types/checkout.ts
@@ -4,7 +4,6 @@ import { CartItem } from './cart-item';
 export interface Checkout {
     cartItems: CartItem[];
     addresses: Address[];
-    deliveryDate: string[];
     shippingFee: number;
     subtotal: number;
     total: number;
