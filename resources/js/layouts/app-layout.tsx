3a1f2d1c3f0278a509b45cd51f93cbb9ad6bf515
ts_dev <writer_fairy_road@yahoo.co.jp>
2026-07-24 22:24:53 +0900

    fix: Remove comment

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -10,22 +10,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
         <div className="flex min-h-dvh flex-col">
             <AppHeader />
 
-            <main className='flex-1'>{children}</main>
+            <main className="flex-1">{children}</main>
         </div>
     );
 }
-
-// Shop layout
-// 〇 shop/index 
-// 〇 shop/product-detail
-// 〇 shop/product-search
-// 〇 cart/index
-// 〇 checkout/index
-// 〇 checkout/success
-// 〇 checkout/failed
-
-// Account layout
-// 〇 account/index
-// 〇 account/address/index
-// 〇 account/order/index
-// 〇 account/security/index
